18df8e94c17a44f76e3e6c31d4b027a423d460e4
ishitvaawasthi <you@example.com>
2020-03-14 07:49:31 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 // void main(){
 //   runApp(MyApp());
 // }
@@ -10,16 +12,16 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var queIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _queIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      queIndex = queIndex + 1;
+      _queIndex = _queIndex + 1;
     });
     print('Answer Chosen');
   }
@@ -37,18 +39,18 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(que.elementAt(queIndex)),
+            Question(que.elementAt(_queIndex)),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
           ],
         ),
